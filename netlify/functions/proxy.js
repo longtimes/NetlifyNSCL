1cabf8fe143ab6f7367d82c502e0d20a26aba46c
Nguyễn Thế Long <thelongol@gmail.com>
2025-11-15 10:33:14 +0700

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,10 +1,10 @@
 export async function handler(event, context) {
   try {
-    const matram = event.queryStringParameters?.matram || '553100';
+    const matram = event.queryStringParameters?.matram || '553600';
     const thoigianbd = event.queryStringParameters?.thoigianbd || "'2025-10-01 00:00:00'";
     const thoigiankt = event.queryStringParameters?.thoigiankt || "'2025-10-01 23:59:00'";
 
-    const apiUrl = `http://203.209.181.170:2018/API_TTB/json/solieu.php?matram=${matram}&ten_table=mucnuoc_oday&sophut=60&tinhtong=0&thoigianbd=${encodeURIComponent(thoigianbd)}&thoigiankt=${encodeURIComponent(thoigiankt)}`;
+    const apiUrl = `http://203.209.181.170:2018/API_TTB/json/solieu.php?matram=${matram}&ten_table=mucnuoc_oday&sophut=10&tinhtong=0&thoigianbd=${encodeURIComponent(thoigianbd)}&thoigiankt=${encodeURIComponent(thoigiankt)}`;
 
     const response = await fetch(apiUrl);
     const data = await response.json();
@@ -21,3 +21,4 @@ export async function handler(event, context) {
     };
   }
 }
+
